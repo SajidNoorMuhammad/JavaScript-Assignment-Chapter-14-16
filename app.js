bac561d4f50735f1a6713fe4b588f9478092b4db
Sajid Noor Muhammad <snoormuhammad37@gmail.com>
2024-03-17 17:10:52 +0500

    Add Work.

--- app.js
+++ app.js
@@ -29,34 +29,36 @@ document.write (`6 ) ${education[5]} <br>`);
 document.write (`7 ) ${education[6]} <br>`);
 document.write (`8 ) ${education[7]} <br>`);
 
+// Question # 08
+var studentName= [prompt("Enter First Name")]
+var studentName1= [prompt("Enter Second Name")]
+var studentName2= [prompt("Enter Third Name")]
+var marks= [prompt("Enter Marks of First Student")]
+var marks1= [prompt("Enter Marks of Second Student")]
+var marks2= [prompt("Enter Marks of Third Student")]
+var total= [(marks / 500 * 100)]
+var total1= [(marks1 / 500 * 100)]
+var total2= [(marks2 / 500 * 100)]
+
+document.write (`Score of ${studentName} is ${marks}. Percentage: ${total}% </br>`);
+document.write (`Score of ${studentName1} is ${marks1}. Percentage: ${total1}% </br>`);
+document.write (`Score of ${studentName2} is ${marks2}. Percentage: ${total2}% </br>`);
+
+var studentName= ["Michael", "John", "Tony"]
+var marks= [320, 230, 480]
+
+document.write(`Score of ${studentName[0]} is ${marks[0]}. Percentage: ${marks[0]/500*100}% </br>`)
+document.write(`Score of ${studentName[1]} is ${marks[1]}. Percentage: ${marks[1]/500*100}% </br>`)
+document.write(`Score of ${studentName[2]} is ${marks[2]}. Percentage: ${marks[2]/500*100}% </br>`)
+
 // Question # 10
-// var studentName= [prompt("Enter First Name")]
-// var studentName1= [prompt("Enter Second Name")]
-// var studentName2= [prompt("Enter Third Name")]
-// var marks= [prompt("Enter Marks of First Student")]
-// var marks1= [prompt("Enter Marks of Second Student")]
-// var marks2= [prompt("Enter Marks of Third Student")]
-// var total= [(marks / 500 * 100)]
-// var total1= [(marks1 / 500 * 100)]
-// var total2= [(marks2 / 500 * 100)]
-
-// document.write (`Score of ${studentName} is ${marks}. Percentage: ${total}% </br>`);
-// document.write (`Score of ${studentName1} is ${marks1}. Percentage: ${total1}% </br>`);
-// document.write (`Score of ${studentName2} is ${marks2}. Percentage: ${total2}% </br>`);
-
-// var studentName= ["Michael", "John", "Tony"]
-// var marks= [320, 230, 480]
-
-// document.write(`Score of ${studentName[0]} is ${marks[0]}. Percentage: ${marks[0]/500*100}% </br>`)
-// document.write(`Score of ${studentName[1]} is ${marks[1]}. Percentage: ${marks[1]/500*100}% </br>`)
-// document.write(`Score of ${studentName[2]} is ${marks[2]}. Percentage: ${marks[2]/500*100}% </br>`)
-
-// Question # 11
 var num= [320, 230, 480, 120]
 
 document.write(`Scores of Students: ${num} <br>`)
 document.write(`Ordered Scores of Students: ${num.sort()} <br>`)
 
+
+// Question # 11
 var city= ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"]
 
 document.write(`${city}<br>`)
